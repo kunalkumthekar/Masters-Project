ad67564d28b26b9a67cc098d7815c7cc61d8724b
Kunal Kumthekar <kkumthekar96@gmail.com>
2020-05-14 22:52:21 +0530

    Compute_overlap and compute_ap methods depecrated

--- yolo_utils.py
+++ yolo_utils.py
@@ -104,58 +104,58 @@ def decode_netout(netout, anchors, img_w, img_h, nb_class, obj_threshold=0.8, nm
     return kpps
 
 # def compute_overlap(a, b):
-    """
-    Code originally from https://github.com/rbgirshick/py-faster-rcnn.
-    Parameters
-    ----------
-    a: (N, 4) ndarray of float
-    b: (K, 4) ndarray of float
-    Returns
-    -------
-    overlaps: (N, K) ndarray of overlap between boxes and query_boxes
-    """
-    area = (b[:, 2] - b[:, 0]) * (b[:, 3] - b[:, 1])
+#     """
+#     Code originally from https://github.com/rbgirshick/py-faster-rcnn.
+#     Parameters
+#     ----------
+#     a: (N, 4) ndarray of float
+#     b: (K, 4) ndarray of float
+#     Returns
+#     -------
+#     overlaps: (N, K) ndarray of overlap between boxes and query_boxes
+#     """
+#     area = (b[:, 2] - b[:, 0]) * (b[:, 3] - b[:, 1])
 
-    iw = np.minimum(np.expand_dims(a[:, 2], axis=1), b[:, 2]) - np.maximum(np.expand_dims(a[:, 0], 1), b[:, 0])
-    ih = np.minimum(np.expand_dims(a[:, 3], axis=1), b[:, 3]) - np.maximum(np.expand_dims(a[:, 1], 1), b[:, 1])
+#     iw = np.minimum(np.expand_dims(a[:, 2], axis=1), b[:, 2]) - np.maximum(np.expand_dims(a[:, 0], 1), b[:, 0])
+#     ih = np.minimum(np.expand_dims(a[:, 3], axis=1), b[:, 3]) - np.maximum(np.expand_dims(a[:, 1], 1), b[:, 1])
 
-    iw = np.maximum(iw, 0)
-    ih = np.maximum(ih, 0)
+#     iw = np.maximum(iw, 0)
+#     ih = np.maximum(ih, 0)
 
-    ua = np.expand_dims((a[:, 2] - a[:, 0]) * (a[:, 3] - a[:, 1]), axis=1) + area - iw * ih
+#     ua = np.expand_dims((a[:, 2] - a[:, 0]) * (a[:, 3] - a[:, 1]), axis=1) + area - iw * ih
 
-    ua = np.maximum(ua, np.finfo(float).eps)
+#     ua = np.maximum(ua, np.finfo(float).eps)
 
-    intersection = iw * ih
+#     intersection = iw * ih
 
-    return intersection / ua
+#     return intersection / ua
 
 # def compute_ap(recall, precision): # never called during training
-    """ Compute the average precision, given the recall and precision curves.
-    Code originally from https://github.com/rbgirshick/py-faster-rcnn.
+#     """ Compute the average precision, given the recall and precision curves.
+#     Code originally from https://github.com/rbgirshick/py-faster-rcnn.
 
-    # Arguments
-        recall:    The recall curve (list).
-        precision: The precision curve (list).
-    # Returns
-        The average precision as computed in py-faster-rcnn.
-    """
-    # correct AP calculation
-    # first append sentinel values at the end
-    mrec = np.concatenate(([0.], recall, [1.]))
-    mpre = np.concatenate(([0.], precision, [0.]))
+#     # Arguments
+#         recall:    The recall curve (list).
+#         precision: The precision curve (list).
+#     # Returns
+#         The average precision as computed in py-faster-rcnn.
+#     """
+#     # correct AP calculation
+#     # first append sentinel values at the end
+#     mrec = np.concatenate(([0.], recall, [1.]))
+#     mpre = np.concatenate(([0.], precision, [0.]))
 
-    # compute the precision envelope
-    for i in range(mpre.size - 1, 0, -1):
-        mpre[i - 1] = np.maximum(mpre[i - 1], mpre[i])
+#     # compute the precision envelope
+#     for i in range(mpre.size - 1, 0, -1):
+#         mpre[i - 1] = np.maximum(mpre[i - 1], mpre[i])
 
-    # to calculate area under PR curve, look for points
-    # where X axis (recall) changes value
-    i = np.where(mrec[1:] != mrec[:-1])[0]
+#     # to calculate area under PR curve, look for points
+#     # where X axis (recall) changes value
+#     i = np.where(mrec[1:] != mrec[:-1])[0]
 
-    # and sum (\Delta recall) * prec
-    ap = np.sum((mrec[i + 1] - mrec[i]) * mpre[i + 1])
-    return ap
+#     # and sum (\Delta recall) * prec
+#     ap = np.sum((mrec[i + 1] - mrec[i]) * mpre[i + 1])
+#     return ap
 
 def _interval_overlap(interval_a, interval_b): # never called during training
     x1, x2 = interval_a
